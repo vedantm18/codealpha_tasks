51f229fbcd57991bf7aac1f2a41a3a4f0abc4995
vedant0303 <vedantmote0303@gmail.com>
2026-01-28 00:20:14 +0530

    Restore DBConnection after merge

--- Stock_Trading_Platform/src/DBConnection.java
+++ Stock_Trading_Platform/src/DBConnection.java
@@ -2,14 +2,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+   private final static String URL="jdbc:mysql://localhost:3306/stock_trading_db";
+   
 
-    private static final String URL =
-        "jdbc:mysql://localhost:3306/stock_trading_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "your_mysql_password";
+   private static final String USER="root";
+   private static final String PASSWORD="vedantsql";
 
-
-    public static Connection getConnection() throws Exception {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+   public static Connection getConnection() throws Exception{
+    return DriverManager.getConnection(URL, USER, PASSWORD);
+   }
+   
+   
 }
